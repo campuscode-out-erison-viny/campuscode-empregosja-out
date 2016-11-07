f795f01b41b6b30af0938979b553778ccdcac7b0
erisonsuzuki <erisonsuzuki@gmail>
2016-11-07 20:33:39 -0200

    Remove category from job

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161107220152) do
+ActiveRecord::Schema.define(version: 20161107223219) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -29,7 +29,6 @@ ActiveRecord::Schema.define(version: 20161107220152) do
 
   create_table "jobs", force: :cascade do |t|
     t.string   "title"
-    t.string   "category"
     t.string   "description"
     t.string   "location"
     t.datetime "created_at",  null: false
