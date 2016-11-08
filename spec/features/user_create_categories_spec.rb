b9c9ca4c60938789a3f28ec72e655e0ec13662f3
Erison&Viny <erisonsuzuki@gmail>
2016-11-08 20:01:39 -0200

    user try to create category that already exist

--- spec/features/user_create_categories_spec.rb
+++ spec/features/user_create_categories_spec.rb
@@ -11,4 +11,25 @@ feature 'User create categories' do
 
     expect(page).to have_content('Categoria')
   end
+
+  scenario 'Category already exists' do
+    category = Category.create(name: 'Categoria')
+
+    visit new_category_path
+
+    fill_in 'Nome', with: 'Categoria'
+
+    click_on 'Criar Categoria'
+
+    expect(page).to have_content('Categoria já utilizada')
+  end
+
+  scenario 'Should fill all fields' do
+    visit new_category_path
+
+    click_on 'Criar Categoria'
+    
+    expect(page).to have_current_path(new_category_path)
+    expect(page) have_content 'Categoria em branco'    
+  end
 end
